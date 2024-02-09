a2b78fe74454220b978451d44269424c2f1659fd
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-02-09 18:52:58 +0200

    Update: basic components, tauri, loading pages and routes

---
+++ app/(landing)/loading.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils";
+
+export default function LoadingPage() {
+    return (
+        <main className='flex flex-col items-center justify-center bg-zinc-100'>
+            <h1 className={cn('text-4xl text-red-800')}>Loading</h1>
+            
+        </main>
+    )
+}
---
+++ app/(welcome)/welcome/page.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface DataType {
+  // TODO
+}
+
+// fetching data in server component (SSR rendering) for SEO and performance
+async function getData() {
+  // TODO fix url with proper data
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+export default async function Page() {
+  const data = await getData()
+  
+  // TODO security check 
+  // TODO redirect to security page if user is not logged in
+
+  return (
+    // child of <main>
+    <span className="flex bg-slate-500 min-h-screen flex-col items-center justify-between p-24">
+      {/* Display a list of items */}
+      <>
+      {data?.map((el: any) => <span key={el.id}>{el.title}</span>)}
+      </>
+      {/* TODO display components */}
+    </span>
+  );
+}
+ 
